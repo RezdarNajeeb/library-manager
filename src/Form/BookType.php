0e2e49ea4af9283b494d9741bfa5cd703c391570
Rezdar Najeeb <rezdar@umggroup.com>
2025-08-28 14:28:40 +0300

    Update BookType form: change publishedAt field to use DateType and add placeholder for category selection

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -7,8 +7,8 @@ use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,18 +17,18 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('author', TextType::class)
-            ->add('isbn', TextType::class)
-            ->add('publishedAt', DateTimeType::class)
+            ->add('title')
+            ->add('author')
+            ->add('isbn')
+            ->add('publishedAt', DateType::class)
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'placeholder' => 'Select a category',
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'Create Book',
             ]);
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
